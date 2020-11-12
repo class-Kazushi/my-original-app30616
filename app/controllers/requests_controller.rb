697984c79211d352df4054763814128ef6e65cf5
class-Kazushi <k.a.z928marine@gmail.com>
2020-11-12 18:54:06 +0900

    依頼編集・更新機能実装

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @requests = Request.order('created_at DESC')
@@ -22,9 +23,29 @@ class RequestsController < ApplicationController
     @request = Request.find(params[:id])
   end
 
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    if @request.update(request_params)
+      redirect_to request_path
+    else
+      render :edit
+    end
+  end
+
   private
   def request_params
     params.require(:request).permit(:title, :category_id, :content, :url, :job, :want, :period, :reword, :prefecture_id, :city, :people, :donation, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @request = Request.find(params[:id])
+    unless @request.user == current_user
+      redirect_to action: :index
+    end
   end
 
 end
