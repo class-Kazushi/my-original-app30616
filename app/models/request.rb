6e6451321347c1a9ae65da7b65f6e2ab9dd143f6
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-14 19:59:54 +0900

    依頼作成　エラーメッセージの日本語化

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,14 +7,14 @@ class Request < ApplicationRecord
   with_options presence: true do
     validates :title, length: { maximum: 10 }
     validates :image
-    validates :category_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1, message: :invalid_id }
     validates :content
     validates :job
     validates :want
-    validates :period
-    validates :reword
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 0, message: :invalid_id }
     validates :city
+    validates :reword
+    validates :period
     validates :people, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 999 }
     validates :donation, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9999999 }
   end
