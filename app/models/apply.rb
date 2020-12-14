a9e80ac51f9d8a619bd559d1a974db8be5555b12
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-14 20:22:13 +0900

    支援ページ　エラーメッセージの日本語化

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -4,6 +4,6 @@ class Apply < ApplicationRecord
   belongs_to :request
 
   validates :reason, presence: true
-  validates :phone_num, format: { with: /\A\d{10}$|^\d{11}\z/ }, allow_blank: true
+  validates :phone_num, format: { with: /\A\d{10}$|^\d{11}\z/, message: :invalid_phone_num }, allow_blank: true
   validates :user_id, uniqueness: { scope: :request_id }
 end
